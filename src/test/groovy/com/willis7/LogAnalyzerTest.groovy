e5187ca1cfe8b52b218d43c355ff03fa0f543c4a
willis7 <sion5@hotmail.co.uk>
2015-06-23 18:03:28 +0100

    Refactoring to parameterised tests. Also called data driven testing in Spock. http://spockframework.github.io/spock/docs/1.0/data_driven_testing.html

--- src/test/groovy/com/willis7/LogAnalyzerTest.groovy
+++ src/test/groovy/com/willis7/LogAnalyzerTest.groovy
@@ -1,41 +1,25 @@
 package com.willis7
 
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * @author Sion Williams
  */
 class LogAnalyzerTest extends Specification {
-    def "IsValidLogFileName bad extension returns false"() {
+
+    @Unroll
+    def "IsValidLogFileName input #value returns #expected"() {
         given:
         LogAnalyzer analyzer = new LogAnalyzer()
 
-        when:
-        boolean result = analyzer.IsValidLogFileName("filewithbadextension.foo")
+        expect:
+        analyzer.IsValidLogFileName(value) == expected
 
-        then:
-        result == false
-    }
-
-    def "IsValidLogFileName good extension lowercase returns true"() {
-        given:
-        LogAnalyzer analyzer = new LogAnalyzer()
-
-        when:
-        boolean result = analyzer.IsValidLogFileName("filewithbadextension.slf")
-
-        then:
-        result == true
-    }
-
-    def "IsValidLogFileName good extension uppercase returns true"() {
-        given:
-        LogAnalyzer analyzer = new LogAnalyzer()
-
-        when:
-        boolean result = analyzer.IsValidLogFileName("filewithbadextension.SLF")
-
-        then:
-        result == true
+        where:
+        value                       | expected
+        "fileWithGoodExtension.SLF" | true
+        "fileWithGoodExtension.slf" | true
+        "fileWithBadExtension.foo"  | false
     }
 }
