25391f823e9e88175d8476b76e6d13ab500deb57
willis7 <sion5@hotmail.co.uk>
2015-06-23 17:07:56 +0100

    Adding some positive tests

--- src/main/java/com/willis7/LogAnalyzer.java
+++ src/main/java/com/willis7/LogAnalyzer.java
@@ -1,12 +1,14 @@
 package com.willis7;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
  * @author Sion Williams
  */
 public class LogAnalyzer {
 
     public boolean IsValidLogFileName(String fileName) {
-        if(!fileName.endsWith(".SLF")) {
+        if(!StringUtils.endsWithIgnoreCase(fileName, ".SLF")) {
             return false;
         }
         return true;
